9458209e25d486d73416854f288e15f55f8a7492
manasaanantham-pixel <manasaanantham@gmail>
2026-08-14 13:09:51 +0530

    Fix production Gmail API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,8 +27,7 @@ import Auth from "./Auth";
 import "./App.css";
 
 
-const API_URL =
-  "http://127.0.0.1:8000";
+const API_URL = "https://cybershieldai-gg60.onrender.com";
 
 
 const emptySummary = {
